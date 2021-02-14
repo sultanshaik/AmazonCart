6e90a4acbe01b0d6583b806cd43560b5a37a4f5b
sultanshaik <sultanshaik.dev@gmail.com>
2021-02-13 23:08:51 -0700

    1 binaries uploaded

--- src/components/cart-items.js
+++ src/components/cart-items.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-function CartItem() {}
+function CartItem(props) {
+  const { imageSource } = props;
+  return (
+    <div className="CartItem">
+      <div className="Product-Image">
+        <img src="https://images.samsung.com/is/image/samsung/assets/ph/p6_gro2/p6_initial_pcd/p6_initial_mobile/ph-pcd-galaxy-m20.png?$ORIGIN_PNG$" />
+      </div>
+      <div className="CartItem-info">
+        <div className="info-title">
+          <h2>Cell Phone</h2>
+          <div className="info-stock">In Stock </div>
+          <div className="item-actions">
+            <div className="item-quantity" />
+            <select />
+            <div className="item-delete">Delete</div>
+          </div>
+        </div>
+      </div>
+      <div className="CartItem-price">$500</div>
+    </div>
+  );
+}
 
 export default CartItem;
--- src/components/shopping-cart.js
+++ src/components/shopping-cart.js
@@ -1,9 +1,12 @@
 import React from "react";
+import CartItem from "./cart-items";
 
 function ShoppingCart() {
   return (
     <div className="Shopping-Main">
       <h1>Shopping Cart</h1>
+      <hr />
+      <CartItem />
     </div>
   );
 }
